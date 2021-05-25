6f8548282f72fd2953479bf900563583e43fb34f
dev-josefeb <febinjose@protonmail.com>
2021-05-25 11:42:21 +0200

    Implement smooth scrolling to features section

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -27,3 +29,7 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+btnScrollTo.addEventListener('click',function(e){
+    section1.scrollIntoView({behavior:"smooth"});
+})
