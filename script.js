8ccc593338f58fdf0d6825279f539674b540a52d
dev-josefeb <febinjose@protonmail.com>
2021-05-25 14:11:31 +0200

    Implement page navigation for feature links by event delegation

--- script.js
+++ script.js
@@ -30,6 +30,15 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-btnScrollTo.addEventListener('click',function(e){
-    section1.scrollIntoView({behavior:"smooth"});
-})
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
