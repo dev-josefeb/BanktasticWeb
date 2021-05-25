73230c89bfee65ed2bdd5fc93fdf86f62213e1a2
dev-josefeb <febinjose@protonmail.com>
2021-05-25 20:48:57 +0200

    Implement fade out effect when hovering over links

--- script.js
+++ script.js
@@ -9,44 +9,39 @@ const section1 = document.querySelector('#section--1');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 
-const openModal = function (e) {
+btnCloseModal.addEventListener('click', closeModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+btnScrollTo.addEventListener('click', scrollSectionIntoView);
+document.addEventListener('keydown', closeModalOnEscPress);
+overlay.addEventListener('click', closeModal);
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+document
+  .querySelector('.nav__links')
+  .addEventListener('click', scrollLinksIntoView);
+tabsContainer.addEventListener('click', switchTabs);
+
+function openModal(e) {
   e.preventDefault();
 
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
-};
+}
 
-const closeModal = function () {
+function closeModal() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-};
+}
 
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
-
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
+function closeModalOnEscPress(e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
-});
+}
 
-btnScrollTo.addEventListener('click', function (e) {
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
-
-document.querySelector('.nav__links').addEventListener('click', function (e) {
-  e.preventDefault();
-
-  if (e.target.classList.contains('nav__link')) {
-    const id = e.target.getAttribute('href');
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-  }
-});
-
-tabsContainer.addEventListener('click', function (e) {
+function switchTabs(e) {
   const clicked = e.target.closest('.operations__tab');
   if (!clicked) return;
 
@@ -56,6 +51,34 @@ tabsContainer.addEventListener('click', function (e) {
   clicked.classList.add('operations__tab--active');
 
   document
-    .querySelector(`.operations__content--${clicked.dataset.tab} `)
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
-});
+}
+
+function handleHover(e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(function (el) {
+      if (el !== link) {
+        el.style.opacity = this;
+        logo.style.opacity = this;
+      }
+    }, this);
+  }
+}
+
+function scrollLinksIntoView(e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+function scrollSectionIntoView(e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+}
